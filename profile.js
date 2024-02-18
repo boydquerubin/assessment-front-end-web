b6ae1708ce147c209e76cefd3457c743ddefe9ab
Boyd Querubin <boydisaacq@gmail.com>
2024-02-18 14:53:14 -0700

    first commit

---
+++ profile.js
@@ -0,0 +1,23 @@
+const color = document.getElementById("color");
+
+const showColor = () => {
+  alert("Green!");
+};
+
+color.addEventListener("click", showColor);
+
+const place = document.getElementById("place");
+
+const showPlace = () => {
+  alert("My bed!");
+};
+
+place.addEventListener("click", showPlace);
+
+const ritual = document.getElementById("ritual");
+
+const showRitual = () => {
+  alert("Listening to LoFi music while coding!");
+};
+
+ritual.addEventListener("click", showRitual);
